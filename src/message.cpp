e93aeb1497e8eb54f9af3f1d0c65cc81516b9d77
MicheleCalliari <michele.calliari04@gmail.com>
2024-05-20 14:40:31 +0200

    ended message.cpp

--- src/message.cpp
+++ src/message.cpp
@@ -9,5 +9,23 @@ bool MessageParser::parse(const std::string& raw_message) {
         return false;
     }
 
+    std::string id_str = raw_message.substr(0, hash_pos); // Estraggo la stringa fino al #
+    std::string payload_str = raw_message.substr(hash_pos + 1);// Estraggo la stringa dopo il #
+
+    //controllo lunghezza stringhe ottenute
+    if (id_str.length() > 3 || payload_str.length() % 2 != 0 || payload_str.length() > 16) {
+        return false;
+    }
+
+    message.id = std::stoul(id_str, nullptr, 16); // converto prima parte del messaggio da esadecimale a intero
+    message.payload.clear(); // svuoto payload di prima
+
+    //completo il payload
+    for (size_t i = 0; i < payload_str.length(); i += 2) {
+        std::string byte_str = payload_str.substr(i, 2);
+        uint8_t byte = static_cast<uint8_t>(std::stoul(byte_str, nullptr, 16));
+        message.payload.push_back(byte);
+    }
+
     return true;
 }
