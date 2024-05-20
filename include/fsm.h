ca80aa716f6a4b5fc64c0702773baf7300e09660
MicheleCalliari <michele.calliari04@gmail.com>
2024-05-20 14:01:36 +0200

    ended fsm.h

--- include/fsm.h
+++ include/fsm.h
@@ -1,4 +1,4 @@
-//fsm.h
+// fsm.h
 #ifndef FSM_H
 #define FSM_H
 
@@ -9,9 +9,19 @@ enum class State {
 
 class FSM {
 public:
+    FSM();
+    void handle_message(const std::string& message);
+    State get_state() const;
 
 private:
+    void transition_to(State new_state);
+    void log_message(const std::string& message);
+    void compute_statistics();
 
+    State current_state;
+    std::ofstream log_file;
+    std::unordered_map<uint16_t, std::vector<uint64_t>> message_timestamps;
+    uint64_t session_start_time;
 };
 
 #endif // FSM_H
