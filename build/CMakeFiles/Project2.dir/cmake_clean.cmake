1166292159628bd084b71c92d81df38a8b64d7fd
MicheleCalliari <michele.calliari04@gmail.com>
2024-05-21 14:40:16 +0200

    setup cmake

---
+++ build/CMakeFiles/Project2.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project2.dir/fake_receiver.cpp.o"
+  "CMakeFiles/Project2.dir/fake_receiver.cpp.o.d"
+  "CMakeFiles/Project2.dir/src/fsm.cpp.o"
+  "CMakeFiles/Project2.dir/src/fsm.cpp.o.d"
+  "CMakeFiles/Project2.dir/src/message.cpp.o"
+  "CMakeFiles/Project2.dir/src/message.cpp.o.d"
+  "CMakeFiles/Project2.dir/src/receiver.cpp.o"
+  "CMakeFiles/Project2.dir/src/receiver.cpp.o.d"
+  "libProject2.dylib"
+  "libProject2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/project_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/mich/Desktop/git/eagletrt-recruiting-sw-master-telemetry-project_2/bin/project_2"
+  "/Users/mich/Desktop/git/eagletrt-recruiting-sw-master-telemetry-project_2/bin/project_2.pdb"
+  "CMakeFiles/project_2.dir/main.cpp.o"
+  "CMakeFiles/project_2.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
